77c5c700b38b16a63c7aa08e3789d01f6d34ad62
Ben S <ogham@bsago.me>
2016-02-18 15:15:44 +0000

    quick-errorify the Parse errors
    You can't seem to use generics with quick-error, but as there were only two uses of the generic enum anyway, I just split them out. And thanks to quick-error, the code is short enough that you can see them right by each other.

--- src/cal/parse.rs
+++ src/cal/parse.rs
@@ -1,5 +1,3 @@
-use std::error::Error as ErrorTrait;
-use std::fmt;
 use std::str::FromStr;
 
 use iso8601;
@@ -10,54 +8,54 @@ use cal::units::{Month, Weekday};
 
 
 impl FromStr for local::Date {
-    type Err = Error<local::Error>;
+    type Err = DateError;
 
     fn from_str(input: &str) -> Result<Self, Self::Err> {
         match iso8601::date(input) {
-            Ok(fields)  => fields_to_date(fields).map_err(Error::Date),
-            Err(e)      => Err(Error::Parse(e)),
+            Ok(fields)  => fields_to_date(fields).map_err(DateError::Construct),
+            Err(e)      => Err(DateError::Parse(e)),
         }
     }
 }
 
 impl FromStr for local::Time {
-    type Err = Error<local::Error>;
+    type Err = DateError;
 
     fn from_str(input: &str) -> Result<local::Time, Self::Err> {
         match iso8601::time(input) {
-            Ok(fields)  => fields_to_time(fields).map_err(Error::Date),
-            Err(e)      => Err(Error::Parse(e)),
+            Ok(fields)  => fields_to_time(fields).map_err(DateError::Construct),
+            Err(e)      => Err(DateError::Parse(e)),
         }
     }
 }
 
 impl FromStr for local::DateTime {
-    type Err = Error<local::Error>;
+    type Err = DateError;
 
     fn from_str(input: &str) -> Result<local::DateTime, Self::Err> {
         let fields = match iso8601::datetime(input) {
             Ok(fields)  => fields,
-            Err(e)      => return Err(Error::Parse(e)),
+            Err(e)      => return Err(DateError::Parse(e)),
         };
 
-        let date = try!(fields_to_date(fields.date).map_err(Error::Date));
-        let time = try!(fields_to_time(fields.time).map_err(Error::Date));
+        let date = try!(fields_to_date(fields.date).map_err(DateError::Construct));
+        let time = try!(fields_to_time(fields.time).map_err(DateError::Construct));
         Ok(local::DateTime::new(date, time))
     }
 }
 
 impl FromStr for offset::DateTime {
-    type Err = Error<offset::Error>;
+    type Err = OffsetError;
 
     fn from_str(input: &str) -> Result<offset::DateTime, Self::Err> {
         let fields = match iso8601::datetime(input) {
             Ok(fields)  => fields,
-            Err(e)      => return Err(Error::Parse(e)),
+            Err(e)      => return Err(OffsetError::Parse(e)),
         };
 
-        let date   = try!(fields_to_date(fields.date).map_err(|e| Error::Date(offset::Error::Date(e))));
-        let time   = try!(fields_to_time(fields.time).map_err(|e| Error::Date(offset::Error::Date(e))));
-        let offset = try!(offset::Offset::of_hours_and_minutes(fields.time.tz_offset_hours as i8, fields.time.tz_offset_minutes as i8).map_err(Error::Date));
+        let date   = try!(fields_to_date(fields.date).map_err(|e| OffsetError::Construct(offset::Error::Date(e))));
+        let time   = try!(fields_to_time(fields.time).map_err(|e| OffsetError::Construct(offset::Error::Date(e))));
+        let offset = try!(offset::Offset::of_hours_and_minutes(fields.time.tz_offset_hours as i8, fields.time.tz_offset_minutes as i8).map_err(OffsetError::Construct));
         Ok(offset.transform_date(local::DateTime::new(date, time)))
     }
 }
@@ -91,33 +89,30 @@ fn fields_to_time(fields: iso8601::Time) -> local::Result<local::Time> {
 }
 
 
-#[derive(PartialEq, Debug, Clone)]
-pub enum Error<E: ErrorTrait> {
-    Date(E),
-    Parse(String),
-}
-
-impl<E: ErrorTrait> fmt::Display for Error<E> {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match *self {
-            Error::Date(ref error)   => write!(f, "{}: {}", self.description(), error),
-            Error::Parse(ref string) => write!(f, "{}: {}", self.description(), string),
+quick_error! {
+    #[derive(PartialEq, Debug, Clone)]
+    pub enum DateError {
+        Construct(err: local::Error) {
+            description("parsing resulted in an invalid date")
+            cause(err)
+        }
+        Parse(desc: String) {
+            description("parse error")
+            display("Parse error: {}", desc)
         }
     }
 }
 
-impl<E: ErrorTrait> ErrorTrait for Error<E> {
-    fn description(&self) -> &str {
-        match *self {
-            Error::Date(_)     => "parsing resulted in an invalid date",
-            Error::Parse(_)    => "parse error",
+quick_error! {
+    #[derive(PartialEq, Debug, Clone)]
+    pub enum OffsetError {
+        Construct(err: offset::Error) {
+            description("parsing resulted in an invalid offset date")
+            cause(err)
         }
-    }
-
-    fn cause(&self) -> Option<&ErrorTrait> {
-        match *self {
-            Error::Date(ref error)   => Some(error),
-            Error::Parse(_)          => None,
+        Parse(desc: String) {
+            description("parse error")
+            display("Parse error: {}", desc)
         }
     }
 }
