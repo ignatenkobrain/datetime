a5dd5c8932c49e65b4e9ce28d5d4d3e17e3cad30
Ben S <ogham@bsago.me>
2016-02-18 22:44:31 +0000

    Inline leap year tests

--- src/cal/unit/year.rs
+++ src/cal/unit/year.rs
@@ -108,3 +108,29 @@ impl AsMut<i64> for Year {
         &mut self.0
     }
 }
+
+
+#[cfg(test)]
+mod test_leap_years {
+    use super::*;
+
+    #[test]
+    fn year_1600() {
+        assert!(Year::from(1600).is_leap_year());
+    }
+
+    #[test]
+    fn year_1900() {
+        assert!(Year::from(1900).is_leap_year() == false);
+    }
+
+    #[test]
+    fn year_2000() {
+        assert!(Year::from(2000).is_leap_year());
+    }
+
+    #[test]
+    fn year_2038() {
+        assert!(Year::from(2038).is_leap_year() == false);
+    }
+}
